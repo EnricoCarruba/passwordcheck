e58065eef5225f35cb93df4d6505e5d97b7ab3cc
Enrico Carruba <carrubaenrico@gmail.com>
2021-10-08 14:55:02 +0200

    add uppercase and lowercase checks

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 // Event listener for inputs
 const input1 = document.getElementById("pw1");
 const input2 = document.getElementById("pw2");
-input1.addEventListener("input", equalCheck);
-input2.addEventListener("input", equalCheck);
+input1.addEventListener("input", pwChecks);
+input2.addEventListener("input", pwChecks);
 // Event listener for button
 const button = document.getElementById("button");
 button.addEventListener("click", toggleButton);
 
+function pwChecks() {
+  equalCheck();
+  if (equalCheck()) {
+    lowercaseCheck();
+    uppercaseCheck();
+  } else {
+    uncheckAll();
+  }
+}
+
 function equalCheck() {
   if (input1.value === input2.value) {
-    console.log("PW EQUAL");
+    const pwEqual = document.getElementById("pw-equal");
+    pwEqual.innerText = "✅";
+    return true;
   } else {
-    console.log("not equal");
+    const pwEqual = document.getElementById("pw-equal");
+    pwEqual.textContent = "❌";
   }
 }
 
@@ -24,5 +37,26 @@ function toggleButton() {
     button.innerText = "Show Passwords";
     input1.type = "password";
     input2.type = "password";
+  }
+}
+
+function lowercaseCheck() {
+  if (input1.value != input1.value.toUpperCase()) {
+    console.log("enthält kleine buchstaben");
+    console.log(input1.value);
+  }
+}
+
+function uppercaseCheck() {
+  if (input1.value != input1.value.toLowerCase()) {
+    console.log("enthält große buchstaben");
+    console.log(input1.value);
+  }
+}
+
+function uncheckAll() {
+  const uncheck = document.querySelectorAll(".checks");
+  for (let i = 0; i < uncheck.length; i++) {
+    uncheck[i].textContent = "❌";
   }
 }
